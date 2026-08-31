5e2bd4087d3aa385d7c96e7389cddad587eb32a5
Aysha Hasan <1220352@student.birzeit.edu>
2026-08-31 22:49:58 +0300

    add nave bar

---
+++ js/script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const navItems = document.querySelectorAll('.nav-item');
+
+    navItems.forEach(item => {
+        item.addEventListener('click', function () {
+            navItems.forEach(navItem =>
+                navItem.classList.remove('active')
+            );
+
+            this.classList.add('active');
+        });
+    });
+});
